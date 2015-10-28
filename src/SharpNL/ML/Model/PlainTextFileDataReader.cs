fe79344a083184da00f2fdef47a42fda6afaa86a
knuppe <gustavoknuppe@gmail.com>
2015-10-28 02:03:39 -0200

    Added encoding support

--- src/SharpNL/ML/Model/PlainTextFileDataReader.cs
+++ src/SharpNL/ML/Model/PlainTextFileDataReader.cs
@@ -1,27 +1,28 @@
-﻿// 
+﻿//  
 //  Copyright 2014 Gustavo J Knuppe (https://github.com/knuppe)
-// 
+//  
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
-// 
+//  
 //       http://www.apache.org/licenses/LICENSE-2.0
-// 
+//  
 //   Unless required by applicable law or agreed to in writing, software
 //   distributed under the License is distributed on an "AS IS" BASIS,
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
-// 
+//  
 //   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //   - May you do good and not evil.                                         -
 //   - May you find forgiveness for yourself and forgive others.             -
 //   - May you share freely, never taking more than you give.                -
 //   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//  
+//   
 
 using System;
 using System.IO;
+using System.Text;
 using SharpNL.Utility;
 
 namespace SharpNL.ML.Model {
@@ -31,21 +32,34 @@ namespace SharpNL.ML.Model {
     public class PlainTextFileDataReader : Disposable, IDataReader {
         private readonly StreamReader reader;
 
-
         /// <summary>
-        /// Initializes a new instance of the <see cref="PlainTextFileDataReader"/> class using a input stream as source.
+        /// Initializes a new instance of the <see cref="PlainTextFileDataReader"/> class.
         /// </summary>
         /// <param name="inputStream">The input stream.</param>
         /// <exception cref="System.ArgumentNullException">inputStream</exception>
-        /// <exception cref="System.ArgumentException">The <paramref name="inputStream"/> was not readable.</exception>
-        public PlainTextFileDataReader(Stream inputStream) {
+        /// <exception cref="System.ArgumentException">The <paramref name="inputStream" /> was not readable.</exception>
+        public PlainTextFileDataReader(Stream inputStream) : this(inputStream, Encoding.UTF8) {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlainTextFileDataReader" /> class using a input stream as source.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <param name="encoding">The text encoding.</param>
+        /// <exception cref="System.ArgumentNullException">inputStream</exception>
+        /// <exception cref="System.ArgumentException">The <paramref name="inputStream" /> was not readable.</exception>
+        public PlainTextFileDataReader(Stream inputStream, Encoding encoding) {
             if (inputStream == null)
                 throw new ArgumentNullException("inputStream");
 
             if (!inputStream.CanRead)
                 throw new ArgumentException(@"Stream was not readable.", "inputStream");
 
-            reader = new StreamReader(inputStream);
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            reader = new StreamReader(inputStream, encoding);
         }
 
         /// <summary>
@@ -61,16 +75,6 @@ namespace SharpNL.ML.Model {
         }
 
         /// <summary>
-        /// Releases the managed resources.
-        /// </summary>
-        protected override void DisposeManagedResources() {
-            base.DisposeManagedResources();
-
-            if (reader != null)
-                reader.Dispose();
-        }
-
-        /// <summary>
         /// Reads a double value.
         /// </summary>
         /// <returns>System.Double.</returns>
@@ -81,14 +85,14 @@ namespace SharpNL.ML.Model {
         /// </exception>
         public double ReadDouble() {
             var line = reader.ReadLine();
-            if (!string.IsNullOrEmpty(line)) {
-                double value;
-                if (!double.TryParse(line, out value)) {
-                    throw new InvalidFormatException("Unable to convert to double the following line: " + line);
-                }
-                return value;
-            }
-            throw new InvalidFormatException("Unable to read a double value.");
+            if (string.IsNullOrEmpty(line))
+                throw new InvalidFormatException("Unable to read a double value.");
+
+            double value;
+            if (!double.TryParse(line, out value))
+                throw new InvalidFormatException("Unable to convert to double the following line: " + line);
+
+            return value;
         }
 
         /// <summary>
@@ -102,14 +106,13 @@ namespace SharpNL.ML.Model {
         /// </exception>
         public int ReadInt() {
             var line = reader.ReadLine();
-            if (!string.IsNullOrEmpty(line)) {
-                int value;
-                if (!int.TryParse(line, out value)) {
-                    throw new InvalidFormatException("Unable to convert to int the following line: " + line);
-                }
-                return value;
-            }
-            throw new InvalidFormatException("Unable to read a int value.");
+            if (string.IsNullOrEmpty(line))
+                throw new InvalidFormatException("Unable to read a int value.");
+
+            int value;
+            if (!int.TryParse(line, out value))
+                throw new InvalidFormatException("Unable to convert to int the following line: " + line);
+            return value;
         }
 
         /// <summary>
@@ -118,6 +121,16 @@ namespace SharpNL.ML.Model {
         /// <returns>System.String.</returns>
         public string ReadString() {
             return reader.ReadLine();
+        }
+
+        /// <summary>
+        /// Releases the managed resources.
+        /// </summary>
+        protected override void DisposeManagedResources() {
+            base.DisposeManagedResources();
+
+            if (reader != null)
+                reader.Dispose();
         }
     }
 }
